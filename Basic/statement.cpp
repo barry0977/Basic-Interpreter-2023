69c0153f1273ea26233b697eac2f56ed553c69aa
Barry <2411557145@qq.com>
2023-12-04 04:02:09 +0800

    Merge pull request #5 from Irfnfnkemed/main
    fix: avoid chmod 777

--- Basic/statement.cpp
+++ Basic/statement.cpp
@@ -20,44 +20,44 @@ Statement::~Statement() = default;
 
 INPUT::INPUT(std::string name)
 {
-    variable=name;
+    variable = name;
 }
 
-INPUT::~INPUT()=default;
+INPUT::~INPUT() = default;
 
-void INPUT::execute(EvalState &state, Program &program)
+void INPUT::execute(EvalState& state, Program& program)
 {
     std::string value;
-    while(true)
+    while (true)
     {
         try
         {
-            std::cout<<" ? ";
-            getline(std::cin,value);
-            int intvalue= stringToInteger(value);
-            state.setValue(variable,intvalue);
+            std::cout << " ? ";
+            getline(std::cin, value);
+            int intvalue = stringToInteger(value);
+            state.setValue(variable, intvalue);
         }
-        catch(ErrorException& ex)
+        catch (ErrorException& ex)
         {
-            std::cout<<"INVALID NUMBER\n";
+            std::cout << "INVALID NUMBER\n";
             continue;
         }
         break;
     }
 }
 
-REM::REM()=default;
+REM::REM() = default;
 
-REM::~REM()=default;
+REM::~REM() = default;
 
-void REM::execute(EvalState &state, Program &program)
+void REM::execute(EvalState& state, Program& program)
 {
     return;
 }
 
 LET::LET(Expression* exp)
 {
-    expression=exp;
+    expression = exp;
 }
 
 LET::~LET()
@@ -65,36 +65,36 @@ LET::~LET()
     delete expression;
 }
 
-void LET::execute(EvalState &state, Program &program)
+void LET::execute(EvalState& state, Program& program)
 {
-    int value=expression->eval(state);
+    int value = expression->eval(state);
 }
 
 GOTO::GOTO(int num)
 {
-    linenumber=num;
+    linenumber = num;
 }
 
-GOTO::~GOTO()=default;
+GOTO::~GOTO() = default;
 
-void GOTO::execute(EvalState &state, Program &program)
+void GOTO::execute(EvalState& state, Program& program)
 {
-    if(program.hasLineNumber(linenumber))
+    if (program.hasLineNumber(linenumber))
     {
-        program.gotoline=linenumber;
+        program.gotoline = linenumber;
     }
     else
     {
-        program.gotoline=-1;
-        std::cout<<"LINE NUMBER ERROR\n";
+        program.gotoline = -1;
+        std::cout << "LINE NUMBER ERROR\n";
     }
 }
 
-IF::IF(Expression* exp,int number,int op)
+IF::IF(Expression* exp, int number, int op)
 {
-    expression=exp;
-    linenumber=number;
-    sign=op;
+    expression = exp;
+    linenumber = number;
+    sign = op;
 }
 
 IF::~IF()
@@ -102,56 +102,56 @@ IF::~IF()
     delete expression;
 }
 
-void IF::execute(EvalState &state, Program &program)
+void IF::execute(EvalState& state, Program& program)
 {
     //std::cout<<"进入IF！\n";
-    Expression *lhs,*rhs;
-    lhs=((CompoundExp *) expression)->getLHS();
-    rhs=((CompoundExp *) expression)->getRHS();
-    int lvalue=lhs->eval(state);
-    int rvalue=rhs->eval(state);
-    bool istrue=false;
-    if(sign==1)
+    Expression* lhs, * rhs;
+    lhs = ((CompoundExp*)expression)->getLHS();
+    rhs = ((CompoundExp*)expression)->getRHS();
+    int lvalue = lhs->eval(state);
+    int rvalue = rhs->eval(state);
+    bool istrue = false;
+    if (sign == 1)
     {
-        if(lvalue>rvalue)
+        if (lvalue > rvalue)
         {
-            istrue=true;
+            istrue = true;
         }
     }
-    else if(sign==-1)
+    else if (sign == -1)
     {
-        if(lvalue<rvalue)
+        if (lvalue < rvalue)
         {
-            istrue=true;
+            istrue = true;
         }
     }
     else
     {
-        if(lvalue==rvalue)
+        if (lvalue == rvalue)
         {
-            istrue=true;
+            istrue = true;
         }
     }
     //std::cout<<"到达if！\n";
-    if(istrue)
+    if (istrue)
     {
-        if(program.hasLineNumber(linenumber))
+        if (program.hasLineNumber(linenumber))
         {
             //std::cout<<"到达第一层！\n";
-            program.gotoline=linenumber;
+            program.gotoline = linenumber;
         }
         else
         {
             //std::cout<<"到达第二层！\n";
-            program.gotoline=-1;
-            std::cout<<"LINE NUMBER ERROR\n";
+            program.gotoline = -1;
+            std::cout << "LINE NUMBER ERROR\n";
         }
     }
 }
 
-PRINT::PRINT(Expression *exp)
+PRINT::PRINT(Expression* exp)
 {
-    expression=exp;
+    expression = exp;
 }
 
 PRINT::~PRINT()
@@ -159,19 +159,19 @@ PRINT::~PRINT()
     delete expression;
 }
 
-void PRINT::execute(EvalState &state, Program &program)
+void PRINT::execute(EvalState& state, Program& program)
 {
-    int value=expression->eval(state);
-    std::cout<<value<<"\n";
+    int value = expression->eval(state);
+    std::cout << value << "\n";
 }
 
-END::END()=default;
+END::END() = default;
 
-END::~END()=default;
+END::~END() = default;
 
-void END::execute(EvalState &state, Program &program)
+void END::execute(EvalState& state, Program& program)
 {
-    program.gotoline=-1;
+    program.gotoline = -1;
 }
 
 
